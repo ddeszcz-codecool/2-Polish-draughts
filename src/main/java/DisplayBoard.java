25a71aaad0f21990a07468c04aac839755cf92ad
Bartek <chesterbachelor@gmail.com>
2022-10-10 14:05:46 +0200

    Added queen functionality

--- src/main/java/DisplayBoard.java
+++ src/main/java/DisplayBoard.java
@@ -1,22 +1,28 @@
 public class DisplayBoard {
-    Pawn [][] gameBoard;
+    Pawn[][] gameBoard;
     int boardLength;
-    DisplayBoard(Pawn[][] board){
+
+    DisplayBoard(Pawn[][] board) {
         gameBoard = board;
         boardLength = gameBoard.length;
     }
-     String whitepiece = "(░)";
-     String blackpiece = "( )";
-     String whitequeen = "[░]";
-     String blackqueen = "[▒]";
-     String blacksquare = "   ";
-     String whitesquare = "▓▓▓";
-     String letters = "ABCDEFGHIJKLMNOPQRST";
+
+    private final String BLACK_BACKGROUND = "\033[48;2;127;66;40m";
+    private final  String BLACK_FOREGROUND = "\033[38;2;0;0;0m";
+    private final  String WHITE_BACKGROUND = "\033[48;2;234;240;174m";
+    private final String WHITE_FOREGROUND = "\033[38;2;255;255;255m";
+    private final String WHITE_PIECE = BLACK_BACKGROUND + "\033[38;5;255m" + "(O)" + WHITE_FOREGROUND;
+    private final  String BLACK_PIECE = BLACK_BACKGROUND + "\033[38;5;232m" + "(O)" + BLACK_FOREGROUND;
+    private final  String WHITE_QUEEN = BLACK_BACKGROUND + "\033[38;5;255m" + "[+]" + WHITE_FOREGROUND;
+    private final String BLACK_QUEEN = BLACK_BACKGROUND + "\033[38;5;232m" + "[+]" + BLACK_FOREGROUND;//"[♚]"
+    private final  String BLACK_SQUARE = BLACK_BACKGROUND + "   " + "\033[0m";
+    private final String WHITE_SQUARE = WHITE_BACKGROUND + "   " + "\033[0m";
+    private final String letters = "ABCDEFGHIJKLMNOPQRST";
 
     public void ShowMeBoard() {
         System.out.print("      ");
         for (int iCol = 0; iCol < boardLength; iCol++) {
-            System.out.print(letters.charAt(iCol)+"  ");
+            System.out.print(letters.charAt(iCol) + "  ");
         }
         System.out.print("\n");
 
@@ -36,9 +42,10 @@ public class DisplayBoard {
 //                        3: whitequeen
 //                        4: whitequeen
 // ==========================================================
-                System.out.print(whitesquare);
-                System.out.print(determineFieldContent(iRow,iCol));
+                System.out.print(WHITE_SQUARE);
+                System.out.print(determineFieldContent(iRow, iCol));
             }
+            System.out.print(formatted);
             System.out.print("\n");
             iRow++;
             formatted = String.format("%3s", (iRow + 1)) + "  ";
@@ -55,23 +62,32 @@ public class DisplayBoard {
 //                        3: whitequeen
 //                        4: whitequeen
 // ==========================================================
-                System.out.print(determineFieldContent(iRow,iCol));
-                System.out.print(whitesquare);
+                System.out.print(determineFieldContent(iRow, iCol));
+                System.out.print(WHITE_SQUARE);
 
             }
+            System.out.print(formatted);
             System.out.print("\n");
         }
+        System.out.print("      ");
+        for (int iCol = 0; iCol < boardLength; iCol++) {
+            System.out.print(letters.charAt(iCol) + "  ");
+        }
+        System.out.print("\n");
 
     }
-    private String determineFieldContent(int row, int col){
-        if(gameBoard[row][col] == null){
-            return blacksquare;
+
+    private String determineFieldContent(int row, int col) {
+        if (gameBoard[row][col] == null) {
+            return BLACK_SQUARE;
         }
 
-        if(gameBoard[row][col].getColor() == Color.BLACK){
-            return blackpiece;
-        }
-        else
-            return whitepiece;
+        if (gameBoard[row][col].getColor() == Color.BLACK) {
+            if (gameBoard[row][col].isCrowned())
+                return BLACK_QUEEN;
+            return BLACK_PIECE;
+        } else if (gameBoard[row][col].isCrowned())
+            return WHITE_QUEEN;
+        return WHITE_PIECE;
     }
 }
