1fd0052af279b336fb1be89641373e3dcbf6449c
MHN468 <MHN468@motorolasolutions.com>
2022-10-13 14:39:47 +0200

    Added 2 templates

--- src/main/java/DisplayBoard.java
+++ src/main/java/DisplayBoard.java
@@ -14,22 +14,23 @@ public class DisplayBoard {
     private static String EMPTY_FOREGROUND = "\033[38;2;234;240;174m";
     private static String WHITE_FOREGROUND = "\033[38;2;255;255;255m";
     private static String SIDE_FOREGROUND = "\033[38;2;213;194;62m";;
-    private final String COLOR_RESET = "\033[0m";
+    private static String COLOR_RESET = "\033[0m";
     private static String BOARD_COLOUR_BACKGROUND = "\033[48;2;130;70;40m";
     private static String BOARD_COLOUR_FOREGROUND = "\033[38;2;130;70;40m";
 
-    private final String WHITE_PIECE = DARK_BACKGROUND + WHITE_FOREGROUND + " \u26C2 " + COLOR_RESET;
-    private final String BLACK_PIECE = DARK_BACKGROUND + BLACK_FOREGROUND + " \u26C2 " + COLOR_RESET;
-    private final String WHITE_QUEEN = DARK_BACKGROUND + WHITE_FOREGROUND + " \u265B " + COLOR_RESET;
-    private final String BLACK_QUEEN = DARK_BACKGROUND + BLACK_FOREGROUND + " \u265B " + COLOR_RESET;
-    private final String BLACK_SQUARE = DARK_BACKGROUND + DARK_FOREGROUND + " \u26C0 " + COLOR_RESET;
-    private final String WHITE_SQUARE = EMPTY_BACKGROUND + EMPTY_FOREGROUND + " \u26C0 " + COLOR_RESET;
+
+    private static String WHITE_PIECE = DARK_BACKGROUND + WHITE_FOREGROUND + " \u26C2 " + COLOR_RESET;
+    private static String BLACK_PIECE = DARK_BACKGROUND + BLACK_FOREGROUND + " \u26C2 " + COLOR_RESET;
+    private static String WHITE_QUEEN = DARK_BACKGROUND + WHITE_FOREGROUND + " \u265B " + COLOR_RESET;
+    private static String BLACK_QUEEN = DARK_BACKGROUND + BLACK_FOREGROUND + " \u265B " + COLOR_RESET;
+    private static String BLACK_SQUARE = DARK_BACKGROUND + DARK_FOREGROUND + " \u26C0 " + COLOR_RESET;
+    private static String WHITE_SQUARE = EMPTY_BACKGROUND + EMPTY_FOREGROUND + " \u26C0 " + COLOR_RESET;
     private final String letters = "ABCDEFGHIJKLMNOPQRST";
-    private final String EMPTY_SPACE = BOARD_COLOUR_FOREGROUND + " \u26C0";
+    private static String EMPTY_SPACE = BOARD_COLOUR_FOREGROUND + " \u26C0";
 
 
     public void ShowMeBoard() {
-
+        chooseTemplate(2);
         System.out.print("\u2004" + BOARD_COLOUR_BACKGROUND + "     \u2008");
         for (int iCol = 0; iCol < boardLength; iCol++) {
             System.out.print(SIDE_FOREGROUND + letters.charAt(iCol) + EMPTY_SPACE);
@@ -79,14 +80,27 @@ public class DisplayBoard {
     private void chooseTemplate(int theme){
         switch (theme){
             case 1:
-
+                //Japanese
+//                DARK_BACKGROUND;
+//                BOARD_COLOUR_BACKGROUND;
+                DARK_FOREGROUND = "\033[38;2;127;66;40m";
                 break;
             case 2:
+                //Sea
+                DARK_BACKGROUND = "\033[48;2;69;114;172m";
+                DARK_FOREGROUND = "\033[38;2;69;114;172m";
+
                 break;
             case 3:
+                //Black frame
+                DARK_BACKGROUND = "\033[48;2;0;0;0m";
+                DARK_FOREGROUND = "\033[38;2;0;0;0m";
                 break;
             case 4:
                 break;
         }
     }
+
+
+
 }
